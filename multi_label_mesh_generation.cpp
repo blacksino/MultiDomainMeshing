f202775deac75d2386e2b2e1e86ed72e0e836d14
xulixin2 <xulixin2@sensetime.com>
2022-08-18 20:16:42 +0800

    fixed boost version bug remeshing added

--- multi_label_mesh_generation.cpp
+++ multi_label_mesh_generation.cpp
@@ -20,6 +20,15 @@ VTK_MODULE_INIT(vtkRenderingFreeType)
 #include <CGAL/facets_in_complex_3_to_triangle_mesh.h>
 #include <CGAL/Mesh_constant_domain_field_3.h>
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Tetrahedral_remeshing/Remeshing_triangulation_3.h>
+#include <CGAL/tetrahedral_remeshing.h>
+#include "/home/SENSETIME/xulixin2/code/cgal-master/Tetrahedral_remeshing/examples/Tetrahedral_remeshing/tetrahedral_remeshing_generate_input.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include <CGAL/IO/output_to_vtu.h>
 #include <CGAL/IO/File_binary_mesh_3.h>
 #include <CGAL/IO/File_medit.h>
@@ -45,6 +54,7 @@ VTK_MODULE_INIT(vtkRenderingFreeType)
 // Domain
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Labeled_mesh_domain_3<K> Mesh_domain;
+typedef CGAL::Tetrahedral_remeshing::Remeshing_triangulation_3<K> Remeshing_triangulation;
 #ifdef CGAL_CONCURRENT_MESH_3
 typedef CGAL::Parallel_tag Concurrency_tag;
 #else
@@ -60,6 +70,7 @@ typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 // To avoid verbose function and named parameters call
 using namespace CGAL::parameters;
 using namespace std;
+//using namespace CGAL;
 typedef typename C3T3::Triangulation::Geom_traits::Point_3             Point;
 typedef std::vector<std::size_t>                                       Face;
 typedef std::vector<Point> Points;
@@ -75,9 +86,30 @@ void user_defined_output_boundary_of_c3t3(const C3T3& c3t3,
     CGAL::Mesh_3::internal::facets_in_complex_3_to_triangle_soup(c3t3, sd_index, visual_points, visual_faces, normals_point_outside_of_the_subdomain);
 }
 
+void remeshing(const std::string file_name,const double target_edge_length)
+
+{
+    const char* file_name_cstr = file_name.c_str();
+    std::ifstream input(file_name_cstr, std::ios_base::in);
+    Remeshing_triangulation tr;
+    CGAL::IO::read_MEDIT(input, tr);
+    // get egdes length of the mesh
+    CGAL::tetrahedral_isotropic_remeshing(tr, target_edge_length);
+    std::ofstream output("output.mesh", std::ios_base::out);
+    CGAL::IO::write_MEDIT(output, tr);
+}
+
 int main(int argc, char* argv[])
 {
-    const std::string fname = (argc>1)?argv[1]:CGAL::data_file_path("/home/SENSETIME/xulixin2/Desktop/merged_liver.inr");
+//    string file_name = "all_liver.mesh";
+//    double target_edge_length = 1;
+//    remeshing(file_name,target_edge_length);
+//    return 0;
+//
+
+
+
+    const std::string fname = (argc>1)?argv[1]:CGAL::data_file_path("/home/SENSETIME/xulixin2/merged_liver.inr");
     std::cout<<fname<<std::endl;
     CGAL::Image_3 image;
     if(!image.read(fname)){
@@ -85,18 +117,20 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
     Mesh_domain domain = Mesh_domain::create_labeled_image_mesh_domain(image);
-    double vessel_size = 3;
-    Sizing_field size(10);
+    double vessel_size = 5;
+
+    Sizing_field size(15);
     size.set_size(vessel_size,3,domain.index_from_subdomain_index(2));
     size.set_size(vessel_size,3,domain.index_from_subdomain_index(1));
+
     // Mesh criteria
-    Mesh_criteria criteria(facet_angle=25, facet_size=4, facet_distance=4,
+    Mesh_criteria criteria(facet_angle=25, facet_size=8, facet_distance=4,
                            cell_radius_edge_ratio=3, cell_size=size);
     C3T3 c3t3 = CGAL::make_mesh_3<C3T3>(domain, criteria);
     CGAL::refine_mesh_3(c3t3,domain,criteria);
     // Output
-    std::ofstream medit_file("all_liver.mesh");
-    std::ofstream vtu_file("all_liver.vtu");
+    std::ofstream medit_file("simple_liver.mesh");
+    std::ofstream vtu_file("simple_liver.vtu");
 
     Points visual_surface_points, visual_interior_vessel_points;
     Faces visual_surface_faces, visual_interior_vessel_faces;
@@ -104,11 +138,10 @@ int main(int argc, char* argv[])
     user_defined_output_boundary_of_c3t3(c3t3,1,visual_interior_vessel_points,visual_interior_vessel_faces);
     c3t3.output_to_medit(medit_file);
     CGAL::IO::output_to_vtu(vtu_file,c3t3,CGAL::IO::ASCII);
-    CGAL::IO::output_to_tetgen("test_tetgen",c3t3);
+    CGAL::IO::output_to_tetgen("simple_tetgen",c3t3);
 
     std::ofstream outFile;
-    outFile.open("test.off");
-
+    outFile.open("simple.off");
 
     // Output
     std::stringstream off_file;
@@ -121,73 +154,7 @@ int main(int argc, char* argv[])
     outFile.close();
 
 
-//    vtkSmartPointer<vtkPolyData> surface_poly_data = vtkSmartPointer<vtkPolyData>::New();
-//    vtkSmartPointer<vtkPolyData> vessel_poly_data = vtkSmartPointer<vtkPolyData>::New();
-//
-//    vtkSmartPointer<vtkPoints> surface_points = vtkSmartPointer<vtkPoints>::New();
-//    vtkSmartPointer<vtkPoints> vessel_points = vtkSmartPointer<vtkPoints>::New();
-//
-//    for(long long i= 0;i<visual_surface_points.size();i++)
-//        surface_points->InsertPoint(i,visual_surface_points[i].x(),visual_surface_points[i].y(),visual_surface_points[i].z());
-//    for(long long i= 0;i<visual_interior_vessel_points.size();i++)
-//        vessel_points->InsertPoint(i,visual_interior_vessel_points[i].x(),visual_interior_vessel_points[i].y(),visual_interior_vessel_points[i].z());
-//
-//    vtkSmartPointer<vtkCellArray> surface_cells = vtkSmartPointer<vtkCellArray>::New();
-//    vtkSmartPointer<vtkCellArray> vessel_cells = vtkSmartPointer<vtkCellArray>::New();
-//
-//    for(size_t i= 0;i<visual_surface_faces.size();i++)
-//    {
-//        vtkIdType id[3] = {static_cast<long long>(visual_surface_faces[i][0]),
-//                           static_cast<long long>(visual_surface_faces[i][1]),
-//                           static_cast<long long>(visual_surface_faces[i][2])};
-//        surface_cells->InsertNextCell(3, id);
-//    }
-//
-//    for(size_t i= 0;i<visual_interior_vessel_faces.size();i++)
-//    {
-//        vtkIdType id[3] = {static_cast<long long>(visual_interior_vessel_faces[i][0]),
-//                           static_cast<long long>(visual_interior_vessel_faces[i][1]),
-//                           static_cast<long long>(visual_interior_vessel_faces[i][2])};
-//        surface_cells->InsertNextCell(3, id);
-//    }
-//
-//
-//    surface_poly_data->SetPoints(surface_points);
-//    surface_poly_data->SetPolys(surface_cells);
-//
-//    vessel_poly_data->SetPoints(vessel_points);
-//    vessel_poly_data->SetPolys(vessel_cells);
 
-//    vtkSmartPointer<vtkAppendPolyData> appendFilter =
-//            vtkSmartPointer<vtkAppendPolyData>::New();
-//    appendFilter->AddInputData(surface_poly_data);
-//    appendFilter->AddInputData(vessel_poly_data);
-//    appendFilter->Update();
-
-//    vtkSmartPointer<vtkPolyDataMapper> mapper =
-//            vtkSmartPointer<vtkPolyDataMapper>::New();
-//    mapper->SetInputData(vessel_poly_data);
-//    vtkSmartPointer<vtkActor>  actor =
-//            vtkSmartPointer<vtkActor>::New();
-//    actor->SetMapper(mapper);
-//    actor->GetProperty()->SetOpacity(0.2);
-//    vtkSmartPointer<vtkRenderer> render =
-//            vtkSmartPointer<vtkRenderer>::New();
-//    render->AddActor(actor);
-//    render->SetBackground(0, 0, 0);
-//
-//    vtkSmartPointer<vtkRenderWindow> rw =
-//            vtkSmartPointer<vtkRenderWindow>::New();
-//    rw->AddRenderer(render);
-//    rw->SetSize(1280, 960);
-//    rw->SetWindowName("PolyData Structure Learning");
-//    rw->Render();
-//
-//    vtkSmartPointer<vtkRenderWindowInteractor> rwi =
-//            vtkSmartPointer<vtkRenderWindowInteractor>::New();
-//    rwi->SetRenderWindow(rw);
-//    rwi->Initialize();
-//    rwi->Start();
 
     return 0;
 }
